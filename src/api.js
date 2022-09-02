d93767a34249294bdf0ee9774fbcc07d82a3db72
McDonald <104572589+McDonaldMusimwa@users.noreply.github.com>
2022-09-02 12:03:19 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '211add2287mshd5e663a0c0a2611p1e2a2cjsn81de55df7dd0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
